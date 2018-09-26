b537562956978101cfd7c6b817c4091a4bf43da8
J <917191299@qq.com>
2018-09-26 22:20:36 +0800

    logback 配置

--- src/main/java/cn/j/sbdemo/SbDemoApplication.java
+++ src/main/java/cn/j/sbdemo/SbDemoApplication.java
@@ -1,9 +1,12 @@
 package cn.j.sbdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//mapper 接口扫描
+@MapperScan("cn.j.sbdemo.*.dao")
 public class SbDemoApplication {
 
 	public static void main(String[] args) {
