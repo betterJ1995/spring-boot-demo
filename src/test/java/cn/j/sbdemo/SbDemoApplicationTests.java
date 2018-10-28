b89838b13e261fbf186bcc8633e6c61b936af52f
J <917191299@qq.com>
2018-10-28 16:56:30 +0800

    Merge branch 'base0' of D:\workspace\mygithub\sb-demo with conflicts.

--- src/test/java/cn/j/sbdemo/SbDemoApplicationTests.java
+++ src/test/java/cn/j/sbdemo/SbDemoApplicationTests.java
@@ -1,15 +1,11 @@
 package cn.j.sbdemo;
 
 import cn.j.sbdemo.sys.dao.SysUserMapper;
-import cn.j.sbdemo.sys.entity.SysUserDo;
-import org.apache.ibatis.session.RowBounds;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import tk.mybatis.mapper.entity.Example;
-import tk.mybatis.mapper.util.Sqls;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,11 +17,11 @@ public class SbDemoApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println("===========");
-        sysUserMapper.selectByExampleAndRowBounds(
-                new Example(SysUserDo.class),
-                new RowBounds(10, 20)
-        );
-        System.out.println(sysUserMapper.selectAll());
+        System.out.println("===========");
+        System.out.println("===========");
+        System.out.println("===========");
+        System.out.println("===========");
+        System.out.println(sysUserMapper.listAll());
     }
 
 }
