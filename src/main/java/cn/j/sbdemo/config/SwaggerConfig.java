0468a922391bc6120c90700e4819607e6758b8be
J <917191299@qq.com>
2018-10-30 23:36:08 +0800

    swagger starer

--- src/main/java/cn/j/sbdemo/config/SwaggerConfig.java
+++ src/main/java/cn/j/sbdemo/config/SwaggerConfig.java
@@ -1,86 +1,86 @@
-package cn.j.sbdemo.config;
-
-import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-/**
- * @Author J
- * @Date 2018/9/27 8:49
- * @Description swagger 接口文档配置
- * 本配置根据包生成接口文档，若需要根据注解生成
- * apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
- **/
-@Configuration
-@EnableSwagger2
-@EnableSwaggerBootstrapUI
-public class SwaggerConfig {
-
-    private ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-
-    /**
-     * UI页面显示信息
-     */
-    private final String API_BASE_PACKAGE1 = "cn.j.sbdemo.sys.api1";
-    private final String API_TITLE = "J 接口文档";
-    private final String API_DESCRIPTION = "J的demo文档描述";
-    private final String API_VERSION = "10.0";
-
-
-    /**
-     * 接口分组一
-     *
-     * @return
-     */
-    @Bean("defaultApi1")
-    public Docket defaultApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                //接口基本信息
-                .apiInfo(
-                        apiInfoBuilder
-                                .title(API_TITLE)
-                                .description(API_DESCRIPTION)
-                                .version(API_VERSION)
-                                .build()
-                )
-                .groupName("defaultApi1")
-                .select()
-                //该包下的接口生成接口文档
-                //注意 扫描的包是以包名为basePackage开头的包
-                //如:配置包 xx.api 若存在xx.api2   xx.api2下的接口也会被扫描进去
-                .apis(RequestHandlerSelectors.basePackage(API_BASE_PACKAGE1))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    /**
-     * 接口分组二
-     *
-     * @return
-     */
-    @Bean("defaultApi2")
-    public Docket defaultApi2() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                //接口基本信息
-                .apiInfo(
-                        apiInfoBuilder
-                                .title(API_TITLE)
-                                .description(API_DESCRIPTION)
-                                .version(API_VERSION)
-                                .build()
-                )
-                .groupName("defaultApi2")
-                .select()
-                //该包下的接口生成接口文档
-                .apis(RequestHandlerSelectors.basePackage("cn.j.sbdemo.sys.api2"))
-                .paths(PathSelectors.any()).build();
-    }
-}
+//package cn.j.sbdemo.config;
+//
+//import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
+//import io.swagger.annotations.ApiOperation;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+///**
+// * @Author J
+// * @Date 2018/9/27 8:49
+// * @Description swagger 接口文档配置
+// * 本配置根据包生成接口文档，若需要根据注解生成
+// * apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+// **/
+//@Configuration
+//@EnableSwagger2
+//@EnableSwaggerBootstrapUI
+//public class SwaggerConfig {
+//
+//    private ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+//
+//    /**
+//     * UI页面显示信息
+//     */
+//    private final String API_BASE_PACKAGE1 = "cn.j.sbdemo.sys.api1";
+//    private final String API_TITLE = "J 接口文档";
+//    private final String API_DESCRIPTION = "J的demo文档描述";
+//    private final String API_VERSION = "10.0";
+//
+//
+//    /**
+//     * 接口分组一
+//     *
+//     * @return
+//     */
+//    @Bean("defaultApi1")
+//    public Docket defaultApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                //接口基本信息
+//                .apiInfo(
+//                        apiInfoBuilder
+//                                .title(API_TITLE)
+//                                .description(API_DESCRIPTION)
+//                                .version(API_VERSION)
+//                                .build()
+//                )
+//                .groupName("defaultApi1")
+//                .select()
+//                //该包下的接口生成接口文档
+//                //注意 扫描的包是以包名为basePackage开头的包
+//                //如:配置包 xx.api 若存在xx.api2   xx.api2下的接口也会被扫描进去
+//                .apis(RequestHandlerSelectors.basePackage(API_BASE_PACKAGE1))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    /**
+//     * 接口分组二
+//     *
+//     * @return
+//     */
+//    @Bean("defaultApi2")
+//    public Docket defaultApi2() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                //接口基本信息
+//                .apiInfo(
+//                        apiInfoBuilder
+//                                .title(API_TITLE)
+//                                .description(API_DESCRIPTION)
+//                                .version(API_VERSION)
+//                                .build()
+//                )
+//                .groupName("defaultApi2")
+//                .select()
+//                //该包下的接口生成接口文档
+//                .apis(RequestHandlerSelectors.basePackage("cn.j.sbdemo.sys.api2"))
+//                .paths(PathSelectors.any()).build();
+//    }
+//}
